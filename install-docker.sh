1aadcd28490269080612b6d4a21bbd06cb0edfbb
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-06-24 20:52:58 +0100

    expense-docker

--- install-docker.sh
+++ install-docker.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 N="\e[0m"
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sydtemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
